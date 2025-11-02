4834e710e53d1298d7d81bf731c90c7a795062f5
N'Dri Anne-Cécilia Myriam Kone <6236961@etu.cegepsth.qc.ca>
2025-11-02 12:57:40 -0500

    Declaraion de nouvelles methodes dans Chunk et ajout d'un booleen IsDirty

--- chunk.h
+++ chunk.h
@@ -11,10 +11,14 @@ public:
     void RemoveBlock(int x, int y, int z);
     void SetBlock(int x, int y, int z, BlockType type);
     BlockType GetBlock(int x, int y, int z) const;
+    void Update();
+    void Render(const Texture &texture, const Shader &shader, const Transformation &transformation);
+    void IsDirty() const;
 
     private: 
     Array3d<BlockType> m_blocks;
     Mesh m_Mesh;
+    bool m_isDirty = true; //Indique si le chunk doit etre regenerer
 };
 
 #endif /* CHUNK_H */
